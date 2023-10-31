4cf64465c38ae2abb061204aefb16bd9d44d59f3
luucuong <luucuong880@gmail.com>
2023-11-01 00:37:58 +0700

    Fix

--- actions/pageObjects/backend/EditReviewPageObject.java
+++ actions/pageObjects/backend/EditReviewPageObject.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import liveguru.backend.BasePage;
 
 public class EditReviewPageObject extends BasePage {
-	private WebDriver driver;
+	WebDriver driver;
 
 	public EditReviewPageObject(WebDriver driver) {
 		this.driver = driver;
